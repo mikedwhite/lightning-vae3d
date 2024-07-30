1f321c1720760f5a62a4b35398de7780773f007d
Mike White <mikedwhite@outlook.com>
2024-07-30 10:57:55 +0100

    feat: 3D version of euler2quaternion added

--- lvae3d/util/Mappings.py
+++ lvae3d/util/Mappings.py
@@ -23,7 +23,7 @@ def euler_distance(a, b):
     return distance
 
 
-def euler2quaternion(euler_angles):
+def euler2quaternion2d(euler_angles):
     """Operates on an array of Euler angles that have been normalised to the range [0, 1].
 
     Parameters
@@ -52,5 +52,38 @@ def euler2quaternion(euler_angles):
     quaternions[:, 1, :, :] = q_x
     quaternions[:, 2, :, :] = q_y
     quaternions[:, 3, :, :] = q_z
+
+    return quaternions
+
+
+def euler2quaternion3d(euler_angles):
+    """Operates on an array of Euler angles that have been normalised to the range [0, 1].
+
+    Parameters
+    ----------
+    euler_angles : torch.Tensor
+        Tensor of normalised Euler angles. Should have shape (batch_size, 3, patch_size, patch_size, patch_size).
+
+    Returns
+    -------
+    quaternions : torch.Tensor
+        Tensor of quaterions with shape (batch_size, 4, patch_size, patch_size, patch_size).
+    """
+
+    quaternions = torch.zeros((euler_angles.shape[0], 4, euler_angles.shape[2], euler_angles.shape[3], euler_angles.shape[4]))
+
+    psi = euler_angles[:, 0, :, :, :] * torch.tensor(math.pi)
+    theta = (euler_angles[:, 1, :, :, :] - (0.5 * torch.tensor(math.pi))) * 0.5 * torch.tensor(math.pi)
+    phi = euler_angles[:, 2, :, :, :] * torch.tensor(math.pi)
+
+    q_w = (torch.cos(phi) * torch.cos(theta) * torch.cos(psi)) + (torch.sin(phi) * torch.sin(theta) * torch.sin(psi))
+    q_x = (torch.sin(phi) * torch.cos(theta) * torch.cos(psi)) - (torch.cos(phi) * torch.sin(theta) * torch.sin(psi))
+    q_y = (torch.cos(phi) * torch.sin(theta) * torch.cos(psi)) + (torch.sin(phi) * torch.cos(theta) * torch.sin(psi))
+    q_z = (torch.cos(phi) * torch.cos(theta) * torch.sin(psi)) - (torch.sin(phi) * torch.sin(theta) * torch.cos(psi))
+
+    quaternions[:, 0, :, :, :] = q_w
+    quaternions[:, 1, :, :, :] = q_x
+    quaternions[:, 2, :, :, :] = q_y
+    quaternions[:, 3, :, :, :] = q_z
 
     return quaternions
