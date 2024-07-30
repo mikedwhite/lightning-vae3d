b60619ecbfae9f35adb7bacb13cf74d523efc99e
Mike White <mikedwhite@outlook.com>
2024-07-30 09:54:03 +0100

    feat: frameworks added for ResNet34 and combining alpha and beta trainers

--- lvae3d/LightningVAETrainers.py
+++ lvae3d/LightningVAETrainers.py
@@ -3,7 +3,7 @@ from torch import nn
 import lightning as L
 from statistics import fmean
 
-from lvae3d.models.ResNetVAE_3D import ResNet18_3DVAE, ResNet18v2_3DVAE
+from lvae3d.models.ResNetVAE_3D import ResNet18_3DVAE, ResNet18v2_3DVAE, ResNet34_3DVAE
 from lvae3d.util.LossFunctions import KLDivergence
 from lvae3d.util.LossFunctions import SpectralLoss3D
 
@@ -15,7 +15,7 @@ class ResNet18VAE_alpha(L.LightningModule):
 
         Parameters
         ----------
-        metadata : lvae3d.util.Metadata
+        metadata : lvae3d.util.MetadataDicts.MetadataAlpha
             Metadata instance from the util.MetadataDicts module.
         loss_func1 : torch.nn.Module
         loss_func2 : torch.nn.Module
@@ -91,7 +91,7 @@ class ResNet18v2VAE_alpha(L.LightningModule):
 
         Parameters
         ----------
-        metadata : lvae3d.util.Metadata
+        metadata : lvae3d.util.MetadataDicts.MetadataAlpha
             Metadata instance from the util.MetadataDicts module.
         loss_func1 : torch.nn.Module
         loss_func2 : torch.nn.Module
@@ -160,6 +160,82 @@ class ResNet18v2VAE_alpha(L.LightningModule):
         return [optimizer]
 
 
+class ResNet34VAE_alpha(L.LightningModule):
+    def __init__(self, metadata, loss_func1=nn.BCEWithLogitsLoss(), loss_func2=SpectralLoss3D):
+        """Lightning trainer for ResNet34_3DVAE. Loss is computed as a weighted sum between two loss functions, such
+        that `loss = alpha * loss_func1 + (1 - alpha) * loss_func2`.
+
+        Parameters
+        ----------
+        metadata : lvae3d.util.MetadataDicts.MetadataAlpha
+            Metadata instance from the util.MetadataDicts module.
+        loss_func1 : torch.nn.Module
+        loss_func2 : torch.nn.Module
+        """
+        super().__init__()
+        self.save_hyperparameters(ignore=['loss_func1', 'loss_func2'])
+        self.metadata = metadata
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.vae = nn.DataParallel(ResNet34_3DVAE(self.metadata.metadata_dict['latent_dim'],
+                                                      self.metadata.metadata_dict['n_channels']))
+        else:
+            self.vae = ResNet34_3DVAE(self.metadata.metadata_dict['latent_dim'],
+                                      self.metadata.metadata_dict['n_channels'])
+        self.loss_func1 = loss_func1
+        self.loss_func2 = loss_func2
+        self.train_loss = []
+        self.val_loss = []
+
+    def forward(self, x):
+        x_hat, mu, log_sigma, z = self.vae(x)
+        return x_hat, mu, log_sigma, z
+
+    def training_step(self, train_batch):
+        x = train_batch['image']
+        x_hat, _, _, _ = self.vae(x)
+        loss1 = self.loss_func1(x, x_hat)
+        loss2 = self.loss_func2(x, x_hat)
+        loss = self.metadata.metadata_dict['alpha'] * loss1 + (1 - self.metadata.metadata_dict['alpha']) * loss2
+        self.train_loss += [loss.item()]
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.log_dict({'train_loss1': loss1, 'train_loss2': loss2, 'train_loss': loss},
+                          on_epoch=True, on_step=False, sync_dist=True)
+        else:
+            self.log_dict({'train_loss1': loss1, 'train_loss2': loss2, 'train_loss': loss},
+                          on_epoch=True, on_step=False)
+        return loss
+
+    def on_train_epoch_end(self):
+        self.metadata.metadata_dict['train_loss'] += [fmean(self.train_loss)]
+        self.train_loss = []
+
+    def validation_step(self, val_batch):
+        x = val_batch['image']
+        x_hat, _, _, _ = self.vae(x)
+        loss1 = self.loss_func1(x, x_hat)
+        loss2 = self.loss_func2(x, x_hat)
+        loss = self.metadata.metadata_dict['alpha'] * loss1 + (1 - self.metadata.metadata_dict['alpha']) * loss2
+        self.val_loss += [loss.item()]
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.log_dict({'val_loss1': loss1, 'val_loss2': loss2, 'val_loss': loss},
+                          on_epoch=True, on_step=False, sync_dist=True)
+        else:
+            self.log_dict({'val_loss1': loss1, 'val_loss2': loss2, 'val_loss': loss},
+                          on_epoch=True, on_step=False)
+
+    def on_validation_epoch_end(self):
+        self.metadata.metadata_dict['val_loss'] += [fmean(self.val_loss)]
+        self.val_loss = []
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(),
+                                     lr=self.metadata.metadata_dict['learning_rate'],
+                                     weight_decay=self.metadata.metadata_dict['weight_decay'],
+                                     amsgrad=self.metadata.metadata_dict['amsgrad']
+                                     )
+        return [optimizer]
+
+
 class ResNet18VAE_beta(L.LightningModule):
     def __init__(self, metadata, loss_func=nn.MSELoss()):
         """Lightning trainer for ResNet18_3DVAE. Loss is computed as a weighted sum between two loss functions, such
@@ -167,7 +243,7 @@ class ResNet18VAE_beta(L.LightningModule):
 
         Parameters
         ----------
-        metadata : lvae3d.util.Metadata
+        metadata : lvae3d.util.MetadataDicts.MetadataBeta
             Metadata instance from the util.MetadataDicts module.
         loss_func1 : torch.nn.Module
         loss_func2 : torch.nn.Module
@@ -242,7 +318,7 @@ class ResNet18v2VAE_beta(L.LightningModule):
 
         Parameters
         ----------
-        metadata : lvae3d.util.Metadata
+        metadata : lvae3d.util.MetadataDicts.MetadataBeta
             Metadata instance from the util.MetadataDicts module.
         loss_func1 : torch.nn.Module
         loss_func2 : torch.nn.Module
@@ -306,5 +382,158 @@ class ResNet18v2VAE_beta(L.LightningModule):
                                      lr=self.metadata.metadata_dict['learning_rate'],
                                      weight_decay=self.metadata.metadata_dict['weight_decay'],
                                      amsgrad=self.metadata.metadata_dict['amsgrad'],
+                                     )
+        return [optimizer]
+
+
+class ResNet34VAE_beta(L.LightningModule):
+    def __init__(self, metadata, loss_func=nn.MSELoss()):
+        """Lightning trainer for ResNet34_3DVAE. Loss is computed as a weighted sum between two loss functions, such
+        that `loss = loss_func + beta * KLDivergence`.
+
+        Parameters
+        ----------
+        metadata : lvae3d.util.MetadataDicts.MetadataBeta
+            Metadata instance from the util.MetadataDicts module.
+        loss_func1 : torch.nn.Module
+        loss_func2 : torch.nn.Module
+        """
+        super().__init__()
+        self.save_hyperparameters(ignore=['loss_func1', 'loss_func2'])
+        self.metadata = metadata
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.vae = nn.DataParallel(ResNet34_3DVAE(self.metadata.metadata_dict['latent_dim'],
+                                                        self.metadata.metadata_dict['n_channels']))
+        else:
+            self.vae = ResNet34_3DVAE(self.metadata.metadata_dict['latent_dim'],
+                                        self.metadata.metadata_dict['n_channels'])
+        self.loss_func = loss_func
+        self.train_loss = []
+        self.val_loss = []
+
+    def forward(self, x):
+        x_hat, mu, log_sigma, z = self.vae(x)
+        return x_hat, mu, log_sigma, z
+
+    def training_step(self, train_batch):
+        x = train_batch['image']
+        x_hat, mu, log_sigma, _ = self.vae(x)
+        loss1 = self.loss_func(x, x_hat)
+        loss2 = KLDivergence()(mu, log_sigma)
+        loss = loss1 + self.metadata.metadata_dict['beta'] * loss2
+        self.train_loss += [loss.item()]
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.log_dict({'train_loss1': loss1, 'train_loss_kl': loss2, 'train_loss': loss},
+                          on_epoch=True, on_step=False, sync_dist=True)
+        else:
+            self.log_dict({'train_loss1': loss1, 'train_loss_kl': loss2, 'train_loss': loss},
+                          on_epoch=True, on_step=False)
+        return loss
+
+    def on_train_epoch_end(self):
+        self.metadata.metadata_dict['train_loss'] += [fmean(self.train_loss)]
+        self.train_loss = []
+
+    def validation_step(self, val_batch):
+        x = val_batch['image']
+        x_hat, mu, log_sigma, _ = self.vae(x)
+        loss1 = self.loss_func(x, x_hat)
+        loss2 = KLDivergence()(mu, log_sigma)
+        loss = loss1 + self.metadata.metadata_dict['beta'] * loss2
+        self.val_loss += [loss.item()]
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.log_dict({'val_loss1': loss1, 'val_loss_kl': loss2, 'val_loss': loss},
+                          on_epoch=True, on_step=False, sync_dist=True)
+        else:
+            self.log_dict({'val_loss1': loss1, 'val_loss_kl': loss2, 'val_loss': loss},
+                          on_epoch=True, on_step=False)
+
+    def on_validation_epoch_end(self):
+        self.metadata.metadata_dict['val_loss'] += [fmean(self.val_loss)]
+        self.val_loss = []
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(),
+                                     lr=self.metadata.metadata_dict['learning_rate'],
+                                     weight_decay=self.metadata.metadata_dict['weight_decay'],
+                                     amsgrad=self.metadata.metadata_dict['amsgrad'],
+                                     )
+        return [optimizer]
+
+
+class ResNet18VAE_alphabeta(L.LightningModule):
+    def __init__(self, metadata, loss_func1=nn.BCEWithLogitsLoss(), loss_func2=SpectralLoss3D):
+        """Lightning trainer for ResNet34_3DVAE. Loss is computed as a weighted sum between two loss functions, such
+        that `loss = alpha * loss_func1 + (1 - alpha) * loss_func2 + beta * KLDivergence`.
+
+        Parameters
+        ----------
+        metadata : lvae3d.util.MetadataDicts.MetadataAlphaBeta
+            Metadata instance from the util.MetadataDicts module.
+        loss_func1 : torch.nn.Module
+        loss_func2 : torch.nn.Module
+        """
+        super().__init__()
+        self.save_hyperparameters(ignore=['loss_func1', 'loss_func2'])
+        self.metadata = metadata
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.vae = nn.DataParallel(ResNet18_3DVAE(self.metadata.metadata_dict['latent_dim'],
+                                                      self.metadata.metadata_dict['n_channels']))
+        else:
+            self.vae = ResNet18_3DVAE(self.metadata.metadata_dict['latent_dim'],
+                                      self.metadata.metadata_dict['n_channels'])
+        self.loss_func1 = loss_func1
+        self.loss_func2 = loss_func2
+        self.train_loss = []
+        self.val_loss = []
+
+    def forward(self, x):
+        x_hat, mu, log_sigma, z = self.vae(x)
+        return x_hat, mu, log_sigma, z
+
+    def training_step(self, train_batch):
+        x = train_batch['image']
+        x_hat, mu, log_sigma, _ = self.vae(x)
+        loss1 = self.loss_func1(x, x_hat)
+        loss2 = self.loss_func2(x, x_hat)
+        loss3 = KLDivergence()(mu, log_sigma)
+        loss = self.metadata.metadata_dict['alpha'] * loss1 + (1 - self.metadata.metadata_dict['alpha']) * loss2 + self.metadata.metadata_dict['beta'] * loss3
+        self.train_loss += [loss.item()]
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.log_dict({'train_loss1': loss1, 'train_loss2': loss2, 'kl_div':loss3, 'train_loss': loss},
+                          on_epoch=True, on_step=False, sync_dist=True)
+        else:
+            self.log_dict({'train_loss1': loss1, 'train_loss2': loss2, 'kl_div':loss3, 'train_loss': loss},
+                          on_epoch=True, on_step=False)
+        return loss
+
+    def on_train_epoch_end(self):
+        self.metadata.metadata_dict['train_loss'] += [fmean(self.train_loss)]
+        self.train_loss = []
+
+    def validation_step(self, val_batch):
+        x = val_batch['image']
+        x_hat, mu, log_sigma, _ = self.vae(x)
+        loss1 = self.loss_func1(x, x_hat)
+        loss2 = self.loss_func2(x, x_hat)
+        loss3 = KLDivergence()(mu, log_sigma)
+        loss = self.metadata.metadata_dict['alpha'] * loss1 + (1 - self.metadata.metadata_dict['alpha']) * loss2 + self.metadata.metadata_dict['beta'] * loss3
+        self.val_loss += [loss.item()]
+        if self.metadata.metadata_dict['parallel'] is True:
+            self.log_dict({'val_loss1': loss1, 'val_loss2': loss2, 'kl_div':loss3, 'val_loss': loss},
+                          on_epoch=True, on_step=False, sync_dist=True)
+        else:
+            self.log_dict({'val_loss1': loss1, 'val_loss2': loss2, 'kl_div':loss3, 'val_loss': loss},
+                          on_epoch=True, on_step=False)
+
+    def on_validation_epoch_end(self):
+        self.metadata.metadata_dict['val_loss'] += [fmean(self.val_loss)]
+        self.val_loss = []
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(),
+                                     lr=self.metadata.metadata_dict['learning_rate'],
+                                     weight_decay=self.metadata.metadata_dict['weight_decay'],
+                                     amsgrad=self.metadata.metadata_dict['amsgrad']
                                      )
         return [optimizer]
--- lvae3d/util/MetadataDicts.py
+++ lvae3d/util/MetadataDicts.py
@@ -152,3 +152,83 @@ class MetadataBeta():
             File name and path to load the metadata dictionary from. Should be specified as a .yaml file.
         """
         self.metadata_dict = yaml.safe_load(Path(path).read_text())
+
+
+class MetadataAlphaBeta():
+    def __init__(self):
+        super().__init__()
+        self.metadata_dict = {}
+
+    def create(self,
+               TrainerModule,
+               loss_func1,
+               loss_func2,
+               alpha,
+               beta,
+               parallel,
+               patch_size,
+               n_channels,
+               n_epochs,
+               learning_rate,
+               weight_decay,
+               batch_size,
+               latent_dim,
+               amsgrad):
+        """Create an instance of the metadata dictionary for the alpha VAE.
+
+        Parameters
+        ----------
+        TrainerModule : lightning.LightningModule
+        loss_func1 : torch.nn.Module
+        loss_func2 : torch.nn.Module
+        alpha : float
+        beta : float
+        parallel : bool
+        patch_size : int
+        n_channels : int
+        n_epochs : int
+        learning_rate : float
+        weight_decay : float
+        batch_size : int
+        latent_dim : int
+        amsgrad : bool
+        """
+        self.metadata_dict = {'trainer': TrainerModule.__name__,
+                              'loss_func1': loss_func1.__class__.__name__,
+                              'loss_func2': loss_func2.__class__.__name__,
+                              'alpha': alpha,
+                              'beta': beta,
+                              'parallel': parallel,
+                              'patch_size': patch_size,
+                              'n_channels': n_channels,
+                              'n_epochs': n_epochs,
+                              'initial_epoch': 0,
+                              'learning_rate': learning_rate,
+                              'weight_decay': weight_decay,
+                              'batch_size': batch_size,
+                              'latent_dim': latent_dim,
+                              'amsgrad': amsgrad,
+                              'train_loss': [],
+                              'val_loss': [],
+                              }
+
+    def save(self, path):
+        """Save metadata dictionary as yaml file.
+
+        Parameters
+        ----------
+        path : str
+            File name and path to save the metadata dictionary. Should be specified as a .yaml file.
+        """
+        with open(path, 'w') as f:
+            yaml.dump(self.metadata_dict, f, default_flow_style=False, sort_keys=False)
+
+    def load(self, path):
+        """Load metadata dictionary from yaml file.
+
+        Parameters
+        ----------
+        path : str
+            File name and path to load the metadata dictionary from. Should be specified as a .yaml file.
+        """
+        self.metadata_dict = yaml.safe_load(Path(path).read_text())
