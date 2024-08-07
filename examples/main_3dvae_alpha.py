b078f1ff84a5a81d9e1d6ec7f22f751f7e7a5b86
Mike White <mikedwhite@outlook.com>
2024-08-07 14:33:39 +0100

    fix: example updated to reflect recent trainer modifications

--- examples/main_3dvae_alpha.py
+++ examples/main_3dvae_alpha.py
@@ -10,7 +10,8 @@ from lightning.pytorch import Trainer, seed_everything
 from lightning.pytorch.callbacks import ModelCheckpoint
 from torchinfo import summary
 
-from lvae3d.LightningVAETrainers import ResNet18VAE_alpha
+from lvae3d.models.ResNetVAE_3D import ResNet18_3DVAE
+from lvae3d.LightningVAETrainers import VAETrainerAlpha
 from lvae3d.util.DataLoaders import Dataset3D, DataModule
 from lvae3d.util.LossFunctions import SpectralLoss3D
 from lvae3d.util.MetadataDicts import MetadataAlpha
@@ -23,27 +24,28 @@ if __name__ == '__main__':
     N_EPOCHS = 10
     LEARNING_RATE = 1e-4
     WEIGHT_DECAY = 1e-4
-    BATCH_SIZE = 32
+    BATCH_SIZE = 8
     LATENT_DIM = 512
     HIDDEN_DIM = 8192
     ALPHA = 0.999
     N_CHANNELS = 3
     PARALLEL = False
     AMSGRAD = True
-    TrainerModule = ResNet18VAE_alpha
+    vae = ResNet18_3DVAE
+    TrainerModule = VAETrainerAlpha
     loss_func1 = nn.BCEWithLogitsLoss()
     loss_func2 = SpectralLoss3D()
 
     # Filepaths
-    TRAIN_DIR = '/path/to/train/data/'
-    VAL_DIR = '/path/to/val/data/'
-    OUT_DIR = f'./out/{TrainerModule.__name__}/latent{LATENT_DIM}/'
+    TRAIN_DIR = './data/train/'
+    VAL_DIR = './data/val/'
+    OUT_DIR = f'./out/{TrainerModule.__name__}/{vae.__class__.__name__}/latent{LATENT_DIM}/'
     if not os.path.exists(OUT_DIR):
         os.makedirs(OUT_DIR)
 
     # Load checkpoint parameters
     load_model = False
-    checkpoint_path = '/path/to/checkpoint.ckpt'
+    checkpoint_path = f'{OUT_DIR}/checkpoint.ckpt'
 
     # Torch config
     seed_everything(42, workers=True)
@@ -56,7 +58,8 @@ if __name__ == '__main__':
         metadata.metadata_dict['initial_epoch'] = metadata.metadata_dict['n_epochs'] + 1
         metadata.metadata_dict['n_epochs'] += N_EPOCHS
     else:
-        metadata.create(TrainerModule=TrainerModule,
+        metadata.create(vae=vae,
+                        TrainerModule=TrainerModule,
                         loss_func1=loss_func1,
                         loss_func2=loss_func2,
                         alpha=ALPHA,
@@ -79,13 +82,14 @@ if __name__ == '__main__':
 
     # Train model
     if load_model:
-        model = TrainerModule.load_from_checkpoint(checkpoint_path,
-                                                   metadata=metadata,
-                                                   loss_func1=loss_func1,
-                                                   loss_func2=loss_func2
-                                                   )
+        model = VAETrainerAlpha.load_from_checkpoint(checkpoint_path,
+                                                     vae=vae,
+                                                     metadata=metadata,
+                                                     loss_func1=loss_func1,
+                                                     loss_func2=loss_func2
+                                                     )
     else:
-        model = TrainerModule(metadata, loss_func1, loss_func2)
+        model = VAETrainerAlpha(vae, metadata, loss_func1, loss_func2)
     summary(model, (1, 3, 64, 64, 64))
 
     checkpoint_callback = ModelCheckpoint(dirpath=OUT_DIR)
@@ -93,10 +97,13 @@ if __name__ == '__main__':
                       max_epochs=N_EPOCHS,
                       default_root_dir=OUT_DIR,
                       callbacks=[checkpoint_callback],
-                      fast_dev_run=True,
-                      accelerator='cpu'
+                      fast_dev_run=False,
+                      accelerator='cpu',
+                      log_every_n_steps=1,
+                      num_sanity_val_steps=0,
                       )
     trainer.fit(model, data_module)
-    trainer.save_checkpoint(filepath=f'{OUT_DIR}checkpoint_weights_epoch{N_EPOCHS}.ckpt', weights_only=True)
+    trainer.save_checkpoint(filepath=f"{OUT_DIR}checkpoint_weights_epoch{metadata.metadata_dict['n_epochs']}.ckpt",
+                            weights_only=True)
 
     metadata.save(f'{OUT_DIR}metadata.yaml')
