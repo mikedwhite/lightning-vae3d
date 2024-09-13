43b58b38910573a56e79d5cc6246be75c9b981e5
Mike White <mikedwhite@outlook.com>
2024-09-13 09:26:12 +0100

    fix: eu2qu refactored

--- lvae3d/util/Mappings.py
+++ lvae3d/util/Mappings.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import math
 
 
+_P = -1
+
+
 def euler_distance(a, b):
     """Compute the element-wise distance between two tensors of Euler angles, `a` and `b`. `a` and `b` must have the
     same shape.
@@ -19,71 +22,72 @@ def euler_distance(a, b):
     distance : torch.Tensor
         Tensor containing the element-wise distance between `a` and `b`.
     """
-    distance = torch.minimum(torch.abs(a - b), 2 * torch.Tensor([math.pi]).type_as(a) - torch.abs(a - b))
+    # distance = torch.minimum(torch.abs(a - b), 2 * torch.Tensor([math.pi]).type_as(a) - torch.abs(a - b))
+    distance = torch.sin(a - b) ** 2
     return distance
 
 
-def euler2quaternion2d(euler_angles):
-    """Operates on an array of Euler angles that have been normalised to the range [0, 1].
+def eu2qu2d(eu):
+    """Operates on a tensor of Euler angles that have been normalised to the range [0, 1].
 
     Parameters
     ----------
-    euler_angles : torch.Tensor
+    eu : torch.Tensor
         Tensor of normalised Euler angles. Should have shape (batch_size, 3, patch_size, patch_size).
 
     Returns
     -------
-    quaternions : torch.Tensor
+    qu : torch.Tensor
         Tensor of quaterions with shape (batch_size, 4, patch_size, patch_size).
     """
 
-    quaternions = torch.zeros((euler_angles.shape[0], 4, euler_angles.shape[2], euler_angles.shape[3]))
+    qu = torch.zeros((eu.shape[0], 4, eu.shape[2], eu.shape[3]))
 
-    psi = euler_angles[:, 0, :, :] * torch.tensor(math.pi)
-    theta = (euler_angles[:, 1, :, :] - (0.5 * torch.tensor(math.pi))) * 0.5 * torch.tensor(math.pi)
-    phi = euler_angles[:, 2, :, :] * torch.tensor(math.pi)
+    psi = eu[:, 0, :, :] * torch.tensor(math.pi)
+    theta = (eu[:, 1, :, :] - (0.5 * torch.tensor(math.pi))) * 0.5 * torch.tensor(math.pi)
+    phi = eu[:, 2, :, :] * torch.tensor(math.pi)
 
     q_w = (torch.cos(phi) * torch.cos(theta) * torch.cos(psi)) + (torch.sin(phi) * torch.sin(theta) * torch.sin(psi))
     q_x = (torch.sin(phi) * torch.cos(theta) * torch.cos(psi)) - (torch.cos(phi) * torch.sin(theta) * torch.sin(psi))
     q_y = (torch.cos(phi) * torch.sin(theta) * torch.cos(psi)) + (torch.sin(phi) * torch.cos(theta) * torch.sin(psi))
     q_z = (torch.cos(phi) * torch.cos(theta) * torch.sin(psi)) - (torch.sin(phi) * torch.sin(theta) * torch.cos(psi))
 
-    quaternions[:, 0, :, :] = q_w
-    quaternions[:, 1, :, :] = q_x
-    quaternions[:, 2, :, :] = q_y
-    quaternions[:, 3, :, :] = q_z
+    qu[:, 0, :, :] = q_w
+    qu[:, 1, :, :] = q_x
+    qu[:, 2, :, :] = q_y
+    qu[:, 3, :, :] = q_z
 
-    return quaternions
+    return qu
 
 
-def euler2quaternion3d(euler_angles):
-    """Operates on an array of Euler angles that have been normalised to the range [0, 1].
+def eu2qu3d(eu):
+    """Operates on a tensor of Euler angles that have been normalised to the range [0, 1].
 
     Parameters
     ----------
-    euler_angles : torch.Tensor
+    eu : torch.Tensor
         Tensor of normalised Euler angles. Should have shape (batch_size, 3, patch_size, patch_size, patch_size).
 
     Returns
     -------
-    quaternions : torch.Tensor
+    qu : torch.Tensor
         Tensor of quaterions with shape (batch_size, 4, patch_size, patch_size, patch_size).
     """
 
-    quaternions = torch.zeros((euler_angles.shape[0], 4, euler_angles.shape[2], euler_angles.shape[3], euler_angles.shape[4]))
+    qu = torch.zeros((eu.shape[0], 4, eu.shape[2], eu.shape[3], eu.shape[4]))
 
-    psi = euler_angles[:, 0, :, :, :] * torch.tensor(math.pi)
-    theta = (euler_angles[:, 1, :, :, :] - (0.5 * torch.tensor(math.pi))) * 0.5 * torch.tensor(math.pi)
-    phi = euler_angles[:, 2, :, :, :] * torch.tensor(math.pi)
+    psi = eu[:, 0, :, :, :] * 2.0 * torch.tensor(math.pi)
+    theta = eu[:, 1, :, :, :] * torch.tensor(math.pi)
+    phi = eu[:, 2, :, :, :] * 2.0 * torch.tensor(math.pi)
 
-    q_w = (torch.cos(phi) * torch.cos(theta) * torch.cos(psi)) + (torch.sin(phi) * torch.sin(theta) * torch.sin(psi))
-    q_x = (torch.sin(phi) * torch.cos(theta) * torch.cos(psi)) - (torch.cos(phi) * torch.sin(theta) * torch.sin(psi))
-    q_y = (torch.cos(phi) * torch.sin(theta) * torch.cos(psi)) + (torch.sin(phi) * torch.cos(theta) * torch.sin(psi))
-    q_z = (torch.cos(phi) * torch.cos(theta) * torch.sin(psi)) - (torch.sin(phi) * torch.sin(theta) * torch.cos(psi))
+    sigma = 0.5 * (psi + phi)
+    delta = 0.5 * (psi - phi)
+    c = torch.cos(0.5 * theta)
+    s = torch.sin(0.5 * theta)
 
-    quaternions[:, 0, :, :, :] = q_w
-    quaternions[:, 1, :, :, :] = q_x
-    quaternions[:, 2, :, :, :] = q_y
-    quaternions[:, 3, :, :, :] = q_z
+    qu[:, 0, :, :, :] = c * torch.cos(sigma)
+    qu[:, 1, :, :, :] = -_P * s * torch.cos(delta)
+    qu[:, 2, :, :, :] = -_P * s * torch.sin(delta)
+    qu[:, 3, :, :, :] = -_P * c * torch.sin(sigma)
 
-    return quaternions
+    return qu
